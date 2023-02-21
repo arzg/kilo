dc6ec852305d0930a0d817a657bba61e6fd11d6e
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 12:51:37 +1100

    Step 19: sections

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,7 +7,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 struct termios orig_termios;
+
+/*** terminal ***/
 
 void die(const char *s) {
 	perror(s);
@@ -33,6 +39,8 @@ void enableRawMode(void) {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
 		die("tcsetattr");
 }
+
+/*** init ***/
 
 int main(void) {
 	enableRawMode();
