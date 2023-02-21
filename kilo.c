bf9e99b768a243197dec7ad529723068792e6291
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 14:47:49 +1100

    Step 47: arrow-keys-enum

--- kilo.c
+++ kilo.c
@@ -15,6 +15,13 @@
 
 #define CTRL_KEY(k) ((k)&0x1f)
 
+enum editorKey {
+	ARROW_LEFT = 'a',
+	ARROW_RIGHT = 'd',
+	ARROW_UP = 'w',
+	ARROW_DOWN = 's'
+};
+
 /*** data ***/
 
 struct editorConfig {
@@ -77,13 +84,13 @@ char editorReadKey(void) {
 		if (seq[0] == '[') {
 			switch (seq[1]) {
 			case 'A':
-				return 'w';
+				return ARROW_UP;
 			case 'B':
-				return 's';
+				return ARROW_DOWN;
 			case 'C':
-				return 'd';
+				return ARROW_RIGHT;
 			case 'D':
-				return 'a';
+				return ARROW_LEFT;
 			}
 		}
 
@@ -208,16 +215,16 @@ void editorRefreshScreen(void) {
 
 void editorMoveCursor(char key) {
 	switch (key) {
-	case 'a':
+	case ARROW_LEFT:
 		E.cx--;
 		break;
-	case 'd':
+	case ARROW_RIGHT:
 		E.cx++;
 		break;
-	case 'w':
+	case ARROW_UP:
 		E.cy--;
 		break;
-	case 's':
+	case ARROW_DOWN:
 		E.cy++;
 		break;
 	}
@@ -233,10 +240,10 @@ void editorProcessKeypress(void) {
 		exit(0);
 		break;
 
-	case 'w':
-	case 's':
-	case 'a':
-	case 'd':
+	case ARROW_UP:
+	case ARROW_DOWN:
+	case ARROW_LEFT:
+	case ARROW_RIGHT:
 		editorMoveCursor(c);
 		break;
 	}
