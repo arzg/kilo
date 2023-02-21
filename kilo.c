b8905f6a94eeebeb0d17f7cf59c9ce457844cf34
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 15:19:03 +1100

    Step 53: home-end-simple

--- kilo.c
+++ kilo.c
@@ -284,6 +284,14 @@ void editorProcessKeypress(void) {
 		exit(0);
 		break;
 
+	case HOME_KEY:
+		E.cx = 0;
+		break;
+
+	case END_KEY:
+		E.cx = E.screencols - 1;
+		break;
+
 	case PAGE_UP:
 	case PAGE_DOWN: {
 		int times = E.screenrows;
