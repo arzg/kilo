fe77712f6b032842d22d09a9a655cb4cfe93a715
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 12:16:08 +1100

    Step 10: ixon

--- kilo.c
+++ kilo.c
@@ -15,6 +15,7 @@ void enableRawMode(void) {
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
+	raw.c_iflag &= ~(IXON);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
