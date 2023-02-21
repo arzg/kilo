8a2d507b6c70b1223bef6a4977e7ce199bfb0f6e
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 13:54:51 +1100

    Step 43: cx-cy

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@
 /*** data ***/
 
 struct editorConfig {
+	int cx, cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -192,6 +193,9 @@ void editorProcessKeypress(void) {
 /*** init ***/
 
 void initEditor(void) {
+	E.cx = 0;
+	E.cy = 0;
+
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
 }
