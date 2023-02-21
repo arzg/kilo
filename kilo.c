9aee262de0a8362c88df1868b4120b2071998d80
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 21:21:21 +1100

    Step 124: insert-newline

--- kilo.c
+++ kilo.c
@@ -318,6 +318,21 @@ void editorInsertChar(int c) {
 	E.cx++;
 }
 
+void editorInsertNewline(void) {
+	if (E.cx == 0) {
+		editorInsertRow(E.cy, "", 0);
+	} else {
+		erow *row = &E.row[E.cy];
+		editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+		row = &E.row[E.cy];
+		row->size = E.cx;
+		row->chars[row->size] = '\0';
+		editorUpdateRow(row);
+	}
+	E.cy++;
+	E.cx = 0;
+}
+
 void editorDelChar(void) {
 	if (E.cy == E.numrows)
 		return;
