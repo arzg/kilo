8a7a0bb130d041e1a7a8abf17f76a9c8cba1472d
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 17:59:42 +1100

    Step 85: rx

--- kilo.c
+++ kilo.c
@@ -43,6 +43,7 @@ typedef struct erow {
 
 struct editorConfig {
 	int cx, cy;
+	int rx;
 	int rowoff;
 	int coloff;
 	int screenrows;
@@ -437,6 +438,7 @@ void editorProcessKeypress(void) {
 void initEditor(void) {
 	E.cx = 0;
 	E.cy = 0;
+	E.rx = 0;
 	E.rowoff = 0;
 	E.coloff = 0;
 	E.numrows = 0;
