3833c772080660292d6e604c345149b1c84ef830
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 20:04:23 +1100

    Step 106: save

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -313,6 +314,20 @@ void editorOpen(char *filename) {
 	}
 	free(line);
 	fclose(fp);
+}
+
+void editorSave(void) {
+	if (E.filename == NULL)
+		return;
+
+	int len;
+	char *buf = editorRowsToString(&len);
+
+	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+	ftruncate(fd, len);
+	write(fd, buf, len);
+	close(fd);
+	free(buf);
 }
 
 /*** append buffer ***/
