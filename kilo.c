1732c3ea274b126f5d3a3380121029a8bf8e7400
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-21 13:24:27 +1100

    Step 31: cursor-query

--- kilo.c
+++ kilo.c
@@ -64,6 +64,25 @@ char editorReadKey(void) {
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+
+	printf("\r\n");
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1) {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
+
+	editorReadKey();
+
+	return -1;
+}
+
 int getWindowSize(int *rows, int *cols) {
 	struct winsize ws;
 
@@ -71,8 +90,7 @@ int getWindowSize(int *rows, int *cols) {
 	    ws.ws_col == 0) {
 		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
 			return -1;
-		editorReadKey();
-		return -1;
+		return getCursorPosition(rows, cols);
 	} else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
