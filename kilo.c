7a5c9d35fa27bc8f69db86d0216606964bed8f67
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-02-23 16:59:14 +1100

    Step 141: search-arrows-help

--- kilo.c
+++ kilo.c
@@ -490,8 +490,8 @@ void editorFind(void) {
 	int saved_coloff = E.coloff;
 	int saved_rowoff = E.rowoff;
 
-	char *query =
-		editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+	char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)",
+				   editorFindCallback);
 
 	if (query) {
 		free(query);
